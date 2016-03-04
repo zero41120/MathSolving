4a916d48b3be4c511573c5b04d092ebef2be33b6
Eric Lin <tlin34@ucsc.edu>
2016-03-04 02:10:37 -0800

    Sum under 100 % by 3 or5

---
+++ Multiples_of_3_and_5/src/Multiples_of_3_and_5.java
@@ -0,0 +1,12 @@
+/* If we list all the natural numbers below 10 that are multiples of 3 or 5,
+ * we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of
+ * all the multiples of 3 or 5 below 1000. */
+
+public class Multiples_of_3_and_5 {
+
+	public static void main(String[] s) {
+		int a = 0, i = 0;
+		while (++i < 1000) a += i % 3 == 0 || i % 5 == 0 ? i : 0;
+		System.out.print(a);
+	}
+}
